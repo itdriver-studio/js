a724985e8048e37b0f8888647a72c1ed15cf570f
Dmitrii <itdriver@yahoo.com>
2024-01-13 17:45:49 +0300

    calc

---
+++ app.js
@@ -0,0 +1,6 @@
+const resultElement = document.getElementById('result');
+const input1 = document.getElementById('input1');
+const input2 = document.getElementById('input2');
+
+const sum = input1.value + input2.value;
+resultElement.textContent = sum;
